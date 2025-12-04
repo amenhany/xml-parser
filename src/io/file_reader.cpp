2fb05eaf458f524ef5a704e5ce0655940338477f
Nadeen Ahmed <163456896+gharablynadeen@users.noreply.github.com>
2025-12-04 19:29:52 +0200

    fileRead function returns string

--- src/io/file_reader.cpp
+++ src/io/file_reader.cpp
@@ -1,5 +1,31 @@
 #include "xml_editor/io.hpp"
+#include <fstream>
+#include <string>
+#include <sstream>  
+#include <iostream>
 
+using namespace std;
 namespace xml_editor::io {
 
+    static std::string fileRead(const std::string &fileName){
+    ifstream file(fileName);
+    if (!file.is_open()) {
+         cerr << "Could not open the file - '" << fileName << "'" << endl;
+         return nullptr ;
+    }
+
+    if (file.peek() == ifstream::traits_type::eof())
+    {
+        cout << "File is empty.";
+        return nullptr ;
+    }
+
+    std::stringstream buffer;
+    buffer << file.rdbuf();   
+    return buffer.str();      
 }
+
+
+    }
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,7 @@
-int main() {
 
+#include <iostream>
+#include "xml_editor/io.hpp"
+int main() {
+    std::cout <<xml_editor::io::fileRead("example.xml")<< std::endl;
+    return 0;
 }
