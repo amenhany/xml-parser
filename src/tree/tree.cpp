cac7e25484603f25524cb878fb56c468fdb666ad
Amen Hany <amen.hany.2810@gmail.com>
2025-12-04 18:47:27 +0200

    Tree method implementations

--- src/tree/tree.cpp
+++ src/tree/tree.cpp
@@ -1,5 +1,21 @@
 #include "xml_editor/tree.hpp"
 
 namespace xml_editor {
+    TreeNode::TreeNode(const std::string& tag, const std::string& value) : tag(tag), value(value), children() {}
 
+    TreeNode::~TreeNode() {
+        for (auto child : children)
+            delete child;
+    }
+
+    void TreeNode::add_child(TreeNode* child) {
+        children.push_back(child);
+    }
+
+
+    Tree::Tree(TreeNode* root) : root(root) {}
+
+    Tree::~Tree() {
+        if (root) delete root;
+    }
 }
