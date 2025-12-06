d000956b59887d62d1c9dd0a71ed6117eb38a2ac
Amen Hany <amen.hany.2810@gmail.com>
2025-12-06 23:31:53 +0200

    Adjusted validator for consistency

--- include/xml_editor/xml.hpp
+++ include/xml_editor/xml.hpp
@@ -1,28 +1,25 @@
 #ifndef XML_EDITOR_XML
 #define XML_EDITOR_XML
 
-#include <iostream>
-#include <fstream>
 #include <string>
-#include <stack>
-#include <vector>
 #include "tree.hpp"
 
-struct XMLError {
+namespace xml_editor::xml {
+
+    struct XMLError {
         int line;
         std::string message;
     };
 
-
-namespace xml_editor::xml {
-
     Tree* parse(const std::string& input);
+
     bool is_valid(const std::string& filename);
-    bool hasErrors();
-    int getErrorCount();
-    const std::vector<XMLError>& getErrors();
-    const std::vector<std::string>& getFixedXML();
-    void printErrors();
+    bool has_errors();
+    int get_error_count();
+    const std::vector<XMLError>& get_errors();
+    const std::vector<std::string>& get_fixed_XML();
+    void print_errors();
+
     void format();
     void to_json();
     void minify();
--- src/xml/validate.cpp
+++ src/xml/validate.cpp
@@ -1,123 +1,126 @@
 #include "xml_editor/xml.hpp"
-
-using namespace std;
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stack>
+#include <vector>
 
 namespace xml_editor::xml {
-  static vector < XMLError > allErrors;
-  static vector < string > fixedLines;
+    static std::vector < XMLError > all_errors;
+    static std::vector < std::string > fixed_lines;
 
-  bool is_valid(const string & filename) {
-    ifstream file(filename); //opens file reads it line by line
-    if (!file.is_open()) {
-      allErrors.push_back({
-        0,
-        "Cannot open file"
-      });
-      return false;
-    }
-
-    stack < string > tags;
-    string line;
-    int lineNumber = 0;
-    allErrors.clear();
-    fixedLines.clear();
-
-    while (getline(file, line)) {
-      lineNumber++;
-      string newLine=line;
-
-      size_t pos = 0;
-
-      while ((pos = line.find('<', pos)) != string::npos) {
-
-        size_t endPos = line.find('>', pos);
-        if (endPos == string::npos) {
-          allErrors.push_back({
-            lineNumber,
-            "Missing '>'"
-          });
-          break;
+    bool is_valid(const std::string& filename) {
+        std::ifstream file(filename); //opens file reads it line by line
+        if (!file.is_open()) {
+            all_errors.push_back({
+              0,
+              "Cannot open file"
+                });
+            return false;
         }
 
-        string tag = line.substr(pos + 1, endPos - pos - 1); //get name inside tag
+        std::stack < std::string > tags;
+        std::string line;
+        int lineNumber = 0;
+        all_errors.clear();
+        fixed_lines.clear();
 
-        if (!tag.size()) {
-          allErrors.push_back({
-            lineNumber,
-            "Empty tag <> detected"
-          });
-          pos = endPos + 1;
-          continue;
+        while (getline(file, line)) {
+            lineNumber++;
+            std::string newLine = line;
+
+            size_t pos = 0;
+
+            while ((pos = line.find('<', pos)) != std::string::npos) {
+
+                size_t endPos = line.find('>', pos);
+                if (endPos == std::string::npos) {
+                    all_errors.push_back({
+                      lineNumber,
+                      "Missing '>'"
+                        });
+                    break;
+                }
+
+                std::string tag = line.substr(pos + 1, endPos - pos - 1); //get name inside tag
+
+                if (!tag.size()) {
+                    all_errors.push_back({
+                      lineNumber,
+                      "Empty tag <> detected"
+                        });
+                    pos = endPos + 1;
+                    continue;
+                }
+
+                if (tag[0] == '/') { //is a closing tag
+                    std::string closing = tag.substr(1);
+
+                    if (tags.empty()) { //nothing in stack,there is a closing tag
+                        all_errors.push_back({
+                          lineNumber,
+                          "Closing tag </" + closing + "> has no opening tag"
+                            });
+                        newLine.insert(pos, "<" + closing + ">"); //add the missing opening tag
+                        pos += closing.size() + 2;
+                        tags.push(closing);
+                    }
+                    if (!tags.empty() && tags.top() != closing) { //closing tag doesn't match top of stack
+                        all_errors.push_back({
+                          lineNumber,
+                          "Mismatched tag: expected </" + tags.top() + "> but found </" + closing + ">"
+                            });
+                        newLine.replace(pos + 1, closing.size(), tags.top()); //added a closing tag for top of stack
+                        closing = tags.top();
+                    }
+                    if (!tags.empty() && tags.top() == closing) { //correct closing tag
+                        tags.pop();
+                    }
+                }
+                else { //was an opening tag --> push in stack
+                    tags.push(tag);
+                }
+
+                pos = endPos + 1;
+            }
+
+            fixed_lines.push_back(newLine);
         }
 
-        if (tag[0] == '/') { //is a closing tag
-          string closing = tag.substr(1);
+        int lastLine = fixed_lines.size();
 
-          if (tags.empty()) { //nothing in stack,there is a closing tag
-            allErrors.push_back({
-              lineNumber,
-              "Closing tag </" + closing + "> has no opening tag"
-            });
-            newLine.insert(pos, "<" + closing + ">"); //add the missing opening tag
-            pos += closing.size() + 2; 
-            tags.push(closing); 
-          } 
-         if (!tags.empty() && tags.top() != closing) { //closing tag doesn't match top of stack
-            allErrors.push_back({
-              lineNumber,
-              "Mismatched tag: expected </" + tags.top() + "> but found </" + closing + ">"
-            });
-            newLine.replace(pos + 1, closing.size(), tags.top()); //added a closing tag for top of stack
-            closing = tags.top();
-          } 
-          if (!tags.empty() && tags.top() == closing) { //correct closing tag
-                    tags.pop(); 
-        }
-        } 
-        else { //was an opening tag --> push in stack
-          tags.push(tag);
+        while (!tags.empty()) { //stack not empty
+            all_errors.push_back({
+              lastLine,
+              "Missing closing tag </" + tags.top() + "> added"
+                });
+            fixed_lines.push_back("</" + tags.top() + ">");
+            tags.pop();
         }
 
-        pos = endPos + 1;
-      }
-
-        fixedLines.push_back(newLine);
+        return all_errors.empty();
     }
 
-    int lastLine = fixedLines.size();
-
-    while (!tags.empty()) { //stack not empty
-      allErrors.push_back({
-        lastLine,
-        "Missing closing tag </" + tags.top() + "> added"
-      });
-      fixedLines.push_back("</" + tags.top() + ">");
-      tags.pop();
+    bool has_errors() {
+        return !all_errors.empty();
     }
 
-    return allErrors.empty();
-  }
-
-  bool hasErrors() {
-    return !allErrors.empty();
-  }
-
-  int getErrorCount() {
-    return allErrors.size();
-  }
-
-  const vector < XMLError > & getErrors() {
-    return allErrors;
-  }
-
-  const vector < string > & getFixedXML() {
-    return fixedLines;
-  }
-
-  void printErrors() {
-    for (auto & e: allErrors) {
-      cout << "Line " << e.line << ": " << e.message << "\n";
+    int get_error_count() {
+        return all_errors.size();
     }
-  }
+
+    const std::vector < XMLError >& get_errors() {
+        return all_errors;
+    }
+
+    const std::vector < std::string >& get_fixed_XML() {
+        return fixed_lines;
+    }
+
+    void print_errors() {
+        for (auto& e : all_errors) {
+            std::cout << "Line " << e.line << ": " << e.message << "\n";
+        }
+    }
 
 }
