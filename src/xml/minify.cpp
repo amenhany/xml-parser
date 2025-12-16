066a7cd7c845b20d94c22584b41e5afeb0fd1724
mhmdz004 <nanzforwork@gmail.com>
2025-12-16 16:33:41 +0200

    Updated minifyXML function

--- src/xml/minify.cpp
+++ src/xml/minify.cpp
@@ -6,61 +6,45 @@ using namespace std;
 namespace xml_editor::xml {
 
 
-string minifyXML(const string& xml) {
-    string result;
-    result.reserve(xml.size());
+    string minifyXML(const string& xml) {
+        string result;
+        result.reserve(xml.size());
 
-    bool insideTag = false;    
-    bool insideText = false;   
-    bool lastWasSpace = false; 
+        bool insideTag = false;
+        bool insideText = false;
 
-    for (size_t i = 0; i < xml.size(); i++) {
-        char c = xml[i];
+        for (size_t i = 0; i < xml.size(); i++) {
+            char c = xml[i];
 
-        if (c == '<') {
-            insideTag = true;
-            insideText = false;
-            result.push_back(c);
-            lastWasSpace = false;
-            continue;
-        }
+            if (c == '<') {
+                insideTag = true;
+                insideText = false;
 
-        if (c == '>') {
-            insideTag = false;
-            insideText = true;
-            result.push_back(c);
-            lastWasSpace = false;
-            continue;
-        }
-
-        if (insideTag) {
-            if (!isspace(c)) {
-                result.push_back(c);
-            }
-            continue;
-        }
-
-        if (insideText) {
-            if (isspace(c)) {
-                if (!lastWasSpace) {
-                    result.push_back(' ');
-                    lastWasSpace = true;
+                while (!result.empty() && isspace((result.back()))) {
+                    result.pop_back();
                 }
-            } else {
+
                 result.push_back(c);
-                lastWasSpace = false;
+                continue;
             }
-            continue;
+
+            if (c == '>') {
+                insideTag = false;
+                insideText = true;
+                result.push_back(c);
+
+                while (i + 1 < xml.size() && isspace((xml[i + 1]))) {
+                    i++;
+                }
+                continue;
+            }
+
+            if (insideTag || insideText) {
+                result.push_back(c);
+            }
         }
+
+        return result;
     }
-
-    if (!result.empty() && result[0] == ' ')
-        result.erase(result.begin());
-
-    if (!result.empty() && result.back() == ' ')
-        result.pop_back();
-
-    return result;
-}
 
 }
