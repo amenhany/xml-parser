ea5fe3c409f2fbe3a1cc9d84c9e7fe1f5fd46d7d
Maged798 <maged.ahmed4@outlook.com>
2025-12-21 23:42:07 +0200

    some modification

--- core/src/sna/get_mutual.cpp
+++ core/src/sna/get_mutual.cpp
@@ -1,32 +1,35 @@
 #include "xml_editor/sna.hpp"
 
 #include <string>
+#include <unordered_set>
 
 namespace xml_editor::sna {
 
     std::string get_mutual(Graph xmlGraph, const std::vector<std::string>& ids) {
+
         std::unordered_map<std::string, User> id_to_user = xmlGraph.get_id_to_user();
-        std::string result = "";
-        User compare_user = id_to_user.at(ids.at(0));
-        std::vector<std::string> compare_followers = compare_user.get_followers();
-        std::vector<std::string> compared_followers;
-        std::vector<std::string> mutual_followers;
+        std::string result;
+        std::unordered_set<std::string> mutual(
+            id_to_user.at(ids[0]).get_followers().begin(),
+            id_to_user.at(ids[0]).get_followers().end()
+        );
+
         for (int i = 1; i < ids.size(); i++) {
-            User compared_user = id_to_user.at(ids.at(i));
-            compared_followers = compared_user.get_followers();
-            for (std::string follower : compared_followers) {
-                for (std::string compare_follower : compare_followers) {
-                    if (follower == compare_follower)
-                        mutual_followers.push_back(follower);
-                }
+            std::unordered_set<std::string> current(
+                id_to_user.at(ids[i]).get_followers().begin(),
+                id_to_user.at(ids[i]).get_followers().end()
+            );
+            for (auto it = mutual.begin(); it != mutual.end(); ) {
+                if (!current.count(*it))
+                    it = mutual.erase(it);
+                else
+                    ++it;
             }
-            compare_followers = mutual_followers;
-            mutual_followers.clear();
         }
         int i = 1;
-        for (std::string mutual_id : compare_followers) {
+        for (std::string mutual_id : mutual) {
             User user = id_to_user.at(mutual_id);
-            result = "User " + std::to_string(i) + ":\n" + "Name: " + user.get_name() + "\nID: " + user.get_id() + "\n\n";
+            result += "User " + std::to_string(i) + ":\n" + "Name: " + user.get_name() + "\n" + "ID: " + user.get_id() + "\n\n";
             i++;
         }
         return result;
--- core/src/sna/get_suggestions.cpp
+++ core/src/sna/get_suggestions.cpp
@@ -1,34 +1,37 @@
 #include "xml_editor/sna.hpp"
+#include <unordered_set>
 
 namespace xml_editor::sna {
+
     std::string get_suggestions(Graph xmlGraph, const std::string& id) {
         std::unordered_map<std::string, User> id_to_user = xmlGraph.get_id_to_user();
         User user = id_to_user.at(id);
-        std::string result = "";
         std::vector<std::string> followers = user.get_followers();
+        std::string result;
+
         if (followers.empty())
             return "No Suggestions";
-        std::vector<std::string> suggestions = id_to_user.at(followers.at(0)).get_followers();
-        for (int i = 1; i < followers.size(); i++) {
-            std::vector<std::string> user_followers = id_to_user.at(followers.at(i)).get_followers();
-            for (std::string follower_id : user_followers) {
-                bool suggested = true;
-                for (std::string suggested_follower : suggestions) {
-                    if (follower_id == suggested_follower || follower_id == id) {
-                        suggested = false;
-                        break;
-                    }
-                }
-                if (suggested)
-                    suggestions.push_back(follower_id);
+
+        std::unordered_set<std::string> suggestions;
+
+        for (std::string follower_id : followers) {
+            for (std::string suggest : id_to_user.at(follower_id).get_followers()) {
+                if (suggest != id)
+                    suggestions.insert(suggest);
             }
         }
+
+        for (std::string follower_id : followers)
+            suggestions.erase(follower_id);
+
         int i = 1;
-        for (std::string suggest : suggestions) {
-            User user = id_to_user.at(suggest);
-            result = "User " + std::to_string(i) + ":\n" + "Name: " + user.get_name() + "\nID: " + user.get_id() + "\n\n";
+        for (std::string s_id : suggestions) {
+            User user = id_to_user.at(s_id);
+            result += "User " + std::to_string(i) + ":\n" + "Name: " + user.get_name() + "\nID: " + user.get_id() + "\n\n";
             i++;
         }
+        if (result.empty())
+            return "No Suggestions";
         return result;
     }
 }
