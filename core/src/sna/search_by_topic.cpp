2d928822b35a6dca04880e3a3319b982f246dd63
Marwan-m-m <2200736@eng.asu.edu.eg>
2025-12-23 14:12:53 +0200

    implemnting search function

--- core/src/sna/search_by_topic.cpp
+++ core/src/sna/search_by_topic.cpp
@@ -2,4 +2,23 @@
 
 namespace xml_editor::sna {
 
+    std::vector<Post> search_by_topic(Graph graph, const std::string& topic) {
+        std::vector<Post> MatchingPosts;
+
+        auto id_to_user = graph.get_id_to_user();
+
+        for (const auto& [user_id, user] : id_to_user) {
+
+            for (const Post& post : user.get_posts()) {
+                for (const std::string& post_topic : post.get_topics()) {
+                    if (post_topic == topic) {
+                        MatchingPosts.push_back(post);
+                        break;
+                    }
+                }
+            }
+        }
+        return MatchingPosts;
+    }
+
 }
--- core/src/sna/search_by_word.cpp
+++ core/src/sna/search_by_word.cpp
@@ -1,5 +1,22 @@
 #include "xml_editor/sna.hpp"
+using namespace std;
 
 namespace xml_editor::sna {
 
+    vector<Post> search_by_word( Graph graph, const string& word) {
+
+        vector <Post> MatchingPosts;
+        auto id_to_user = graph.get_id_to_user();
+
+        for (const auto& [user_id, user] : id_to_user) {
+
+            for (const Post& post : user.get_posts()) {
+                
+                if (post.get_text().find(word) != string::npos) {
+                    MatchingPosts.push_back(post);
+                }
+            }
+        }
+        return MatchingPosts;
+    }
 }
