b660d5e78bc49069279bfca46654b8d80483be2b
Maged798 <maged.ahmed4@outlook.com>
2025-12-24 23:15:40 +0200

    most_active includes all edges

--- core/src/sna/most_active.cpp
+++ core/src/sna/most_active.cpp
@@ -4,18 +4,18 @@ namespace xml_editor::sna {
 
     const User* most_active(const Graph& xmlGraph) {
         const auto& graph = xmlGraph.get_graph();
+        const auto& id_to_user = xmlGraph.get_id_to_user();
         int max = 0;
         std::string max_id;
 
         for (const auto& user : graph) {
-            if (user.second.size() > max) {
-                max = user.second.size();
+            if (user.second.size() + (id_to_user.at(user.first)).get_followers().size() > max) {
+                max = user.second.size() + (id_to_user.at(user.first)).get_followers().size();
                 max_id = user.first;
             }
         }
         if (max == 0)
             return nullptr;
-        const auto& id_to_user = xmlGraph.get_id_to_user();
         return &id_to_user.at(max_id);
     }
 }
